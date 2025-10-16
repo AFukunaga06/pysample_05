8fe114c52b1834a71cb19ef7892c399334f162f8
AFukunaga06 <afky5906@gmail.com>
2025-10-16 15:13:48 +0900

    作業内容を保存2

--- testBS0711.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.example.com"
-res = requests.get(url)
-soup = BeautifulSoup(res.text, "html.parser")
-print("ページタイトル：", soup.title.string)
